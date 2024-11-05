a5a5dc3e4c2e465a428e9fc4d4b0a0899ac8cf92
João Meneses <meneses-joao@hotmail.com>
2024-11-04 22:12:05 -0300

    updates

--- js/home.js
+++ js/home.js
@@ -1,5 +1,6 @@
-const selectElement = (element) => document.querySelector(element); 
+const selectElement = (element) => document.querySelector(element);
 
 selectElement('header').addEventListener('click', () => {
     selectElement('.mobile-menu').classList.toggle('active');
+    selectElement('header').classList.toggle('expanded');
 });
