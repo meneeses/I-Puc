d08fe1894a9f61a38079f4f1bce6c5bc64262a59
Lucas Gabriel Kuligowski <lucaskuligowski@gmail.com>
2024-11-07 21:36:13 -0300

    footer feito

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ function loadHeaderFooter(){
     fetch("components/header.html")
     .then(response => response.text())
     .then(data => document.getElementById("header").innerHTML = data);
+
+    fetch("components/footer.html")
+    .then(response => response.text())
+    .then(data => document.getElementById("footer").innerHTML = data);
 }
 
 
